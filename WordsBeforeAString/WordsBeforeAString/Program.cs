fa93c3967c17fc5edb3dbd9c6eeae41c239fd755
jjepson <jjepson@gmail.com>
2016-06-10 11:23:20 -0700

    Add files via upload

---
+++ WordsBeforeAString/WordsBeforeAString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordsBeforeAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "The quick brown fox jumps over the lazy dog";
+            string word = "over";
+            Console.WriteLine(sentence);
+            Console.WriteLine(word);
+            Console.WriteLine(GetNum(sentence, word).ToString());
+            Console.ReadLine();
+        }
+
+        static long GetNum(string sentence, string word)
+        {
+            int tmpWordLocation = sentence.IndexOf(word);
+            int tmpSpaceCount = sentence.Substring(0,tmpWordLocation).Count(x => x == ' ');
+
+            return tmpSpaceCount;
+        }
+    }
+}
