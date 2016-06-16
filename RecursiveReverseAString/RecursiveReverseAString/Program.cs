c85933005d975a4788df0e9cb09aa43a3ec87974
jjepson <jjepson@gmail.com>
2016-06-15 21:03:30 -0700

    Add files via upload

---
+++ RecursiveReverseAString/RecursiveReverseAString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecursiveReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine(ReverseString("Test"));
+            Console.ReadLine();
+        }
+
+        static string ReverseString(string original)
+        {
+            if (original.Length <= 1)
+            {
+                return original;
+            }
+            else
+            {
+                return ReverseString(original.Substring(1))+original[0];
+            }
+        }
+    }
+}
